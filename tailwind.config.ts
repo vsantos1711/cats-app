d35008cca17ee4453717874389f19d7dbad93e13
vsantos1711 <vsantos067100@gmail.com>
2023-12-24 21:49:53 -0300

    feat :add next themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "2rem",
+        sm: "4rem",
+        lg: "8rem",
+        xl: "10rem",
+        "2xl": "12rem",
       },
     },
+    extend: {},
   },
-  plugins: [],
-}
-export default config
+};
+export default config;
