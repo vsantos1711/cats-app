97f6ba66aa87e0091d4da023630f00e3a444b3e8
vsantos1711 <vsantos067100@gmail.com>
2023-12-25 02:12:28 -0300

    style: remove unnecessary styles

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -2,7 +2,7 @@ import Photo from "./Photo";
 
 export default function Feed() {
   return (
-    <main className="dark:bg-slate-700  min-h-screen container">
+    <main className="container">
       <Photo />
     </main>
   );
