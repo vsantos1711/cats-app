162666438225050b453e8c11de0e50ca58b3cd6d
vsantos1711 <vsantos067100@gmail.com>
2023-12-25 02:12:53 -0300

    fix: fix path to not found svg

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,25 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
+import notFound from "../assets/svg/not-found.svg";
 
 export default function NotFound() {
   return (
-    <div className="container flex flex-col pt-10 items-center min-h-screen gap-4">
-      <h1 className="sm:text-4xl text-2xl font-bold text-center ">
-        404: Página não encontrada
-      </h1>
-      <Image
-        src="/svg/status/not-found.svg"
-        alt="Cat walking"
-        width={600}
-        height={600}
-      />
-      <p className="sm:text-2xl text-lg font-bold text-center max-w-[700px]">
-        Ops! Parece que um gato curioso passou por aqui e levou essa página!
-        Estamos rastreando suas pegadas. 🐱🐾
-      </p>
-      <Link href="/" className="text-2xl">
-        Voltar para casa
-      </Link>
+    <div className="py-10 min-h-screen dark:bg-slate-700">
+      <div className="container flex flex-col items-center gap-4">
+        <h1 className="sm:text-4xl text-2xl font-bold text-center ">
+          404: Página não encontrada
+        </h1>
+        <Image src={notFound} alt="Cat walking" width={600} height={600} />
+        <p className="sm:text-2xl text-lg font-bold text-center max-w-[700px]">
+          Ops! Parece que um gato curioso passou por aqui e levou essa página!
+          Estamos rastreando suas pegadas. 🐱🐾
+        </p>
+        <Link href="/" className="text-2xl">
+          Voltar para casa
+        </Link>
+      </div>
     </div>
   );
 }
