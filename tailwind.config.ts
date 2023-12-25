ea4833c13f110c9125f9c79168b2d2002fa9058f
vsantos1711 <vsantos067100@gmail.com>
2023-12-25 02:01:17 -0300

    style: create keyframe fade in

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,21 @@ const config: Config = {
         "2xl": "12rem",
       },
     },
+    keyframes: {
+      "fade-in": {
+        "0%": {
+          opacity: "0",
+          transform: "scale(0.5)",
+        },
+        "100%": {
+          opacity: "1",
+          transform: "scale(1)",
+        },
+      },
+    },
+    animation: {
+      "fade-in": "fade-in 0.3s ease",
+    },
     extend: {},
   },
 };
