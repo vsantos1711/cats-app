2bb7a3f2569245cf9e8eee92a5d12cc98631b861
vsantos1711 <vsantos067100@gmail.com>
2023-12-25 16:34:57 -0300

    refactor: Photo component refactor and add tv

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,71 +1,39 @@
-import Image from "next/image";
-import catOne from "../assets/img/cat-one.jpg";
-import catTwo from "../assets/img/cat-two.jpg";
-import catThree from "../assets/img/cat-three.jpg";
-import catFour from "../assets/img/cat-four.jpg";
-import catFive from "../assets/img/cat-five.jpg";
-import catSix from "../assets/img/cat-six.jpg";
-// import catSeven from "../assets/img/cat-seven.jpg";
-// import catEight from "../assets/img/cat-eight.jpg";
-// import catNine from "../assets/img/cat-nine.jpg";
-// import catTen from "../assets/img/cat-ten.jpg";
-// import catEleven from "../assets/img/cat-eleven.jpg";
-// import catTwelve from "../assets/img/cat-twelve.jpg";
+import Image, { StaticImageData } from "next/image";
+import { ComponentProps } from "react";
+import { tv, VariantProps } from "tailwind-variants";
 
-export default function Photo() {
+const photo = tv({
+  base: "relative w-full min-h-60",
+  variants: {
+    size: {
+      "xg-top-right": "md:row-start-1 md:row-end-3 md:col-start-2 md:col-end-4",
+      "xg-top-left": "md:row-start-1 md:row-end-3 md:col-start-1 md:col-end-3",
+      "xg-bot-right": "md:row-start-2 md:row-end-4 md:col-start-2 md:col-end-4",
+      "xg-bot-left": "md:row-start-2 md:row-end-4 md:col-start-1 md:col-end-3",
+    },
+  },
+});
+
+type Photo = ComponentProps<"img"> &
+  VariantProps<typeof photo> & {
+    url: string | StaticImageData;
+    alt?: string;
+  };
+
+export default function Photo({
+  size,
+  url,
+  alt = "a cata image",
+  className,
+}: Photo) {
   return (
-    <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
-      <div className="relative w-full min-h-60 ">
-        <Image
-          src={catOne}
-          alt="a cat image"
-          fill
-          className="object-cover rounded animate-fade-in hover:opacity-75 hover:scale-105  dark:border-none shadow-lg"
-        />
-      </div>
-
-      <div className="relative w-full min-h-60 ">
-        <Image
-          src={catTwo}
-          alt="a cat image"
-          fill
-          className="object-cover rounded animate-fade-in hover:opacity-75 hover:scale-105  dark:border-none shadow-lg"
-        />
-      </div>
-
-      <div className="relative w-full min-h-60 md:row-start-1 md:row-end-3 md:col-start-2 md:col-end-4">
-        <Image
-          src={catThree}
-          alt="a cat image"
-          fill
-          className="object-cover rounded animate-fade-in hover:opacity-75 hover:scale-105  dark:border-none shadow-lg"
-        />
-      </div>
-
-      <div className="relative w-full min-h-60 ">
-        <Image
-          src={catFour}
-          alt="a cat image"
-          fill
-          className="object-cover rounded animate-fade-in hover:opacity-75 hover:scale-105  dark:border-none shadow-lg"
-        />
-      </div>
-      <div className="relative w-full min-h-60 ">
-        <Image
-          src={catFive}
-          alt="a cat image"
-          fill
-          className="object-cover rounded animate-fade-in hover:opacity-75 hover:scale-105  dark:border-none shadow-lg"
-        />
-      </div>
-      <div className="relative w-full min-h-60 ">
-        <Image
-          src={catSix}
-          alt="a cat image"
-          fill
-          className="object-cover rounded animate-fade-in hover:opacity-75 hover:scale-105  dark:border-none shadow-lg"
-        />
-      </div>
+    <div className={photo({ size, className })}>
+      <Image
+        src={url}
+        alt={alt}
+        fill
+        className="object-cover rounded animate-fade-in hover:opacity-75 hover:scale-105  dark:border-none shadow-lg"
+      />
     </div>
   );
 }
