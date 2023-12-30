ab3c270a6e4a00c4455844389f2650781485abc9
vsantos1711 <vsantos067100@gmail.com>
2023-12-29 22:23:09 -0300

    feat: create SignIn and SignUp schemas

---
+++ src/lib/types.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const signUpSchema = z
+  .object({
+    username: z.string().min(3, "Username must be at least 3 characters"),
+    email: z.string().email(),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+export type TSignUpSchema = z.infer<typeof signUpSchema>;
+
+export const signInSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+});
+
+export type TSignInSchema = z.infer<typeof signInSchema>;
