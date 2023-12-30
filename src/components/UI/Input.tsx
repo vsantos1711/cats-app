a0889f5d57929e30e6dd8521919640d130a74bc4
vsantos1711 <vsantos067100@gmail.com>
2023-12-29 22:22:43 -0300

    feat: update input component with react hook form

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -1,19 +1,31 @@
+import { UseFormRegisterReturn } from "react-hook-form";
+
 type InputProps = {
   label: string;
   id: string;
   type: string;
+  placeholder?: string;
+  register: UseFormRegisterReturn;
 };
 
-export default function Input({ id, label, type }: InputProps) {
+export default function Input({
+  id,
+  label,
+  type,
+  placeholder,
+  register,
+}: InputProps) {
   return (
-    <div className="flex flex-col pb-5 gap-1">
+    <div className="flex flex-col gap-1 pb-1">
       <label htmlFor={id} className="font-normal">
         {label}
       </label>
       <input
         className="bg-[#E8F0FE] duration-300 rounded p-2 hover:ring-2 ring-[#3d397c] dark:ring-[#4badce] border dark:text-black max-w-[450px]"
+        placeholder={placeholder}
         type={type}
         id={id}
+        {...register}
       />
     </div>
   );
