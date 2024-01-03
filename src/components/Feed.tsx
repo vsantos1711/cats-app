564cf06d2cb5d7b5c05ae999de745392450a0ad2
vsantos1711 <vsantos067100@gmail.com>
2024-01-02 21:51:40 -0300

    feat: add mock views on Photos

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -14,20 +14,20 @@ export default function Feed() {
   return (
     <main className="container flex flex-col gap-4 pt-2">
       <div className="grid md:grid-cols-3 grid-cols-2 gap-4 ">
-        <Photo url={catOne} />
-        <Photo url={catTwo} />
-        <Photo url={catFive} />
-        <Photo url={catTwo} />
-        <Photo url={catFour} />
-        <Photo url={catNine} variant="xg-bot-right" />
+        <Photo url={catOne} views="123123" />
+        <Photo url={catTwo} views="2387" />
+        <Photo url={catFive} views="4242" />
+        <Photo url={catTwo} views="235" />
+        <Photo url={catFour} views="523523" />
+        <Photo url={catNine} variant="xg-bot-right" views="1463263" />
       </div>
       <div className="grid md:grid-cols-3 grid-cols-2 gap-4 ">
-        <Photo url={catSeven} />
-        <Photo url={catEight} />
-        <Photo url={catTen} />
-        <Photo url={catTen} />
-        <Photo url={catThree} variant="xg-top-left" />
-        <Photo url={catTen} />
+        <Photo url={catSeven} views="24234" />
+        <Photo url={catEight} views="23465234" />
+        <Photo url={catTen} views="234234" />
+        <Photo url={catTen} views="2387" />
+        <Photo url={catThree} variant="xg-top-left" views="2387" />
+        <Photo url={catTen} views="134143" />
       </div>
     </main>
   );
