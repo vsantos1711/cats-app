bc8598edf2b3c51c37dc82e6bac74122c4624676
vsantos1711 <vsantos067100@gmail.com>
2024-01-08 22:14:08 -0300

    feat: use feed with a list of static photos

--- src/components/feed.tsx
+++ src/components/feed.tsx
@@ -11,23 +11,85 @@ import catNine from "../assets/img/cat-nine.jpg";
 import catTen from "../assets/img/cat-ten.jpg";
 
 export default function Feed() {
+  const itemsPageOne = [
+    {
+      url: catOne,
+      views: "123123",
+      variant: "top-right",
+    },
+    {
+      url: catTwo,
+      views: "2387",
+    },
+    {
+      url: catThree,
+      views: "2387",
+    },
+    {
+      url: catFour,
+      views: "523523",
+    },
+    {
+      url: catFive,
+      views: "4242",
+    },
+    {
+      url: catSix,
+      views: "234234",
+    },
+  ];
+  const itemsPageTwo = [
+    {
+      url: catSeven,
+      views: "24234",
+      variant: "top-right",
+    },
+    {
+      url: catEight,
+      views: "23465234",
+    },
+    {
+      url: catNine,
+      views: "1463263",
+    },
+    {
+      url: catTen,
+      views: "234234",
+    },
+    {
+      url: catEight,
+      views: "23465234",
+    },
+    {
+      url: catNine,
+      views: "1463263",
+    },
+    {
+      url: catTen,
+      views: "234234",
+    },
+  ];
   return (
-    <main className="container flex flex-col gap-4 pt-2">
-      <div className="grid md:grid-cols-3 grid-cols-2 gap-4 ">
-        <Photo url={catOne} views="123123" />
-        <Photo url={catTwo} views="2387" />
-        <Photo url={catFive} views="4242" />
-        <Photo url={catTwo} views="235" />
-        <Photo url={catFour} views="523523" />
-        <Photo url={catNine} variant="xg-bot-right" views="1463263" />
+    <main className="container flex flex-col max-w-screen-xl gap-4 pt-2 ">
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
+        {itemsPageOne.map((item: any, index) => (
+          <Photo
+            key={index}
+            url={item.url}
+            views={item.views}
+            variant={item.variant}
+          />
+        ))}
       </div>
-      <div className="grid md:grid-cols-3 grid-cols-2 gap-4 ">
-        <Photo url={catSeven} views="24234" />
-        <Photo url={catEight} views="23465234" />
-        <Photo url={catTen} views="234234" />
-        <Photo url={catTen} views="2387" />
-        <Photo url={catThree} variant="xg-top-left" views="2387" />
-        <Photo url={catTen} views="134143" />
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
+        {itemsPageTwo.map((item: any, index) => (
+          <Photo
+            key={index}
+            url={item.url}
+            views={item.views}
+            variant={item.variant}
+          />
+        ))}
       </div>
     </main>
   );
