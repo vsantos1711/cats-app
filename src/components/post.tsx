a8a8394a0404258e45632090055c1a3fdce36b29
vsantos1711 <vsantos067100@gmail.com>
2024-01-14 22:08:00 -0300

    style: add dark mode on the Post component

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -6,6 +6,7 @@ import { Button } from "./ui/button";
 import { useRef } from "react";
 import { FaEye } from "react-icons/fa";
 import Logo from "./logo";
+import Link from "next/link";
 
 type PostProps = {
   post: PostData;
@@ -29,13 +30,18 @@ export default function Post({ post, onclick }: PostProps) {
       className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-white/1"
       onClick={handleClick}
     >
-      <div className="flex flex-col md:flex-row rounded overflow-hidden m-3 w-[900px] h-[600px] bg-[#f0f0f0]">
+      <div className="flex flex-col md:flex-row rounded overflow-scroll m-3 w-[900px] h-[600px] bg-[#f0f0f0] dark:bg-slate-600 ">
         <div className="relative md:w-[62%] md:min-h-full min-h-[65%]">
           <Image alt="cat" src={post.url} fill objectFit="cover" />
         </div>
         <div className="flex flex-col flex-1 gap-2 p-5 ">
-          <div className="flex justify-between w-full mb-4 text-sm text-gray-400">
-            <p className="underline">@{post.author}</p>
+          <div className="flex justify-between w-full mb-4 text-sm text-gray-400 dark:text-gray-300">
+            <Link
+              className="underline hover:text-blue-700 dark:hover:text-white"
+              href="#"
+            >
+              @{post.author}
+            </Link>
             <p className="flex items-center gap-1 ">
               <FaEye size={15} />
               <span>{post.views}</span>
@@ -44,9 +50,9 @@ export default function Post({ post, onclick }: PostProps) {
           <div className="flex items-end justify-between mb-2 ">
             <h2 className="text-3xl text-decoration">{post.catInfos.name}</h2>
             <div className="flex items-center gap-1 text-end ">
-              <p className="text-sm text-gray-400 ">
+              <p className="text-sm text-gray-400 text-nowrap ">
                 Breed:{" "}
-                <span className="text-black">
+                <span className="text-black dark:text-white ">
                   {post.catInfos.breed ?? "N/A"}
                 </span>
               </p>
@@ -55,37 +61,44 @@ export default function Post({ post, onclick }: PostProps) {
           <div className="flex items-center gap-4 mb-8">
             <div className="flex items-center gap-1 ">
               <p className="text-sm text-gray-400 ">
-                Years: <span className="text-black">{post.catInfos.age}</span>
+                Years:{" "}
+                <span className="text-black dark:text-white ">
+                  {post.catInfos.age}
+                </span>
               </p>
             </div>
 
             <div className="flex items-center gap-1">
               <p className="text-sm text-gray-400 ">
                 Weight:{" "}
-                <span className="text-black">{post.catInfos.weight}kg</span>
+                <span className="text-black dark:text-white ">
+                  {post.catInfos.weight}kg
+                </span>
               </p>
             </div>
           </div>
 
           <div className="flex flex-col gap-2 ">
-            <ScrollArea className="p-1 border rounded-md h-80">
+            <ScrollArea className="p-1 rounded-md h-80">
               {post.comments?.map((comment, index) => (
-                <div key={index} className="font-semibold ">
-                  <p>
-                    {comment.author}:{" "}
-                    <span className="font-light"> {comment.text}</span>
-                  </p>
-                </div>
+                <p key={index} className="my-0.5 font-semibold ">
+                  {comment.author}:{" "}
+                  <span className="font-light"> {comment.text}</span>
+                </p>
               ))}
             </ScrollArea>
           </div>
           <div className="flex items-center justify-between h-full ">
             <Textarea
-              className="p-1 bg-gray-200 rounded w-60"
+              className="p-1 bg-gray-200 rounded w-60 dark:bg-slate-500 dark:border-gray-500"
               placeholder="Add a comment..."
             />
-            <Button variant="ghost" size="icon">
-              <Logo className="transition hover:text-gray-700" />
+            <Button
+              variant="ghost"
+              size="icon"
+              className="text-gray-400 transition dark:text-gray-300 hover:bg-transparent hover:text-black dark:hover:text-white dark:hover:bg-transparent"
+            >
+              <Logo />
             </Button>
           </div>
         </div>
