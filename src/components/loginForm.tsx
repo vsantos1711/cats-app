a6610070934957cc3d4b9f95714d0408f7b992f1
vsantos1711 <vsantos067100@gmail.com>
2024-01-25 20:49:00 -0300

    feat: add toast on login form

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -11,30 +11,42 @@ import { useTranslations } from "next-intl";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { TLoginSchema, loginSchema } from "@/lib/types";
-
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { userLogin } from "@/services/api/user/user-login.service";
 import { useRouter } from "@/navigation";
 import { userStore } from "@/store/user-store";
+import { useToast } from "./ui/use-toast";
 
 export default function LoginForm() {
   const t = useTranslations("LoginPage");
   const router = useRouter();
+  const user = userStore();
+  const { toast } = useToast();
 
   const form = useForm<TLoginSchema>({
     resolver: zodResolver(loginSchema),
   });
 
   const onSubmit = async (data: TLoginSchema) => {
-    const res = await userLogin(data);
-
-    userStore.setState({
-      username: res.username,
-      accessToken: res.accessToken,
-    });
-
-    if (res) router.push("/");
+    try {
+      const res = await userLogin(data);
+      user.setUserName(res.username);
+      toast({
+        title: "Login",
+        description: "Login realizado com sucesso!",
+        duration: 2000,
+      });
+      router.push("/");
+    } catch (error) {
+      toast({
+        title: "Erro",
+        description: "Something gone wrong!",
+        variant: "destructive",
+        duration: 2000,
+      });
+      console.error("Erro ao fazer login:", error);
+    }
   };
 
   return (
@@ -48,7 +60,7 @@ export default function LoginForm() {
               <FormItem>
                 <FormLabel>{t("label-one")}</FormLabel>
                 <FormControl>
-                  <Input {...field} />
+                  <Input type="text" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
