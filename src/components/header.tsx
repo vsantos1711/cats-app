88d352f2bf70fa6dcd404b19c90275b4c4e62695
vsantos1711 <vsantos067100@gmail.com>
2024-01-27 11:47:01 -0300

    feat: show username on the header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,26 +1,25 @@
-"use client";
 import { Link } from "@/navigation";
 import Logo from "./logo";
 import { ToggleTheme } from "./toggleTheme";
 import { useTranslations } from "next-intl";
-import { userStore } from "@/store/user-store";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { cookies } from "next/headers";
 
 export default function Header() {
   const t = useTranslations("Header");
-  const userData = userStore((state) => state.username);
+  const username = cookies().get("username")?.value;
   return (
     <header className="relative flex transition duration-300 bg-gray-100 border h-14 dark:border-none dark:bg-gray-800">
       <nav className="container flex items-center justify-between text-gray-800 dark:text-gray-200 ">
         <Link href="/">
           <Logo size="sm" />
         </Link>
-        {userData ? (
+        {username ? (
           <Link
-            href={`/user/${userData}`}
-            className="flex items-center gap-2 text-lg font-medium"
+            href={`/user/${username}`}
+            className="flex items-center gap-3 text-lg font-medium"
           >
-            {userData}
+            {username}
             <Avatar>
               <AvatarImage src="https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg" />
               <AvatarFallback>CN</AvatarFallback>
