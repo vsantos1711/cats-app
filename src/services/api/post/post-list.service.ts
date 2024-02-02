742ab191f29ed7d149291c66559b2d7be8ac2511
vsantos1711 <vsantos067100@gmail.com>
2024-02-01 22:20:31 -0300

    feat: add type

--- src/services/api/post/post-list.service.ts
+++ src/services/api/post/post-list.service.ts
@@ -1,6 +1,7 @@
+import { IPost } from "@/types/post";
 import { api } from "../api";
 
-export const postList = async (): Promise<any[]> => {
+export const postList = async (): Promise<IPost[]> => {
   const { data } = await api.get("/post/findAll");
   return data;
 };
